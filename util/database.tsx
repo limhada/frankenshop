7cc70c13b26384ca8844dc13cb81da0f1d13eec0
limhada <leem1315@gmail.com>
2023-08-02 16:30:00 +0900

    Commit 복구

--- util/database.tsx
+++ util/database.tsx
@@ -3,7 +3,7 @@ import { MongoClient, MongoClientOptions } from 'mongodb';
 
 const url = process.env.NEXT_PUBLIC_DB_URL || '';
 const options = { useNewUrlParser: true };
-let connectDB: Promise<MongoClient>; // 수정된 타입을 지정
+let connectDB: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongo) {
