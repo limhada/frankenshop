ffc4555b3d1a9a1b4d73b9a4a7c5471782f2bb69
limhada <leem1315@gmail.com>
2023-08-08 17:38:16 +0900

    초기 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -13,8 +12,20 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        // mycolor1: '#112321',
+        // mycolor1: '#617143',
+        // mycolor1: '#41644A',
+        // mycolor1: '#5B9A8B',
+        // mycolor1: '#7EAA92',
+        // mycolor1: '#7C9D96',
+        // mycolor1: '#A1CCD1',
+        // mycolor1: '#8D7B68',
+        // mycolor1: '#A4907C',
+        mycolor1: '#F4F2DE',
+        // mycolor1: '#445069',
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
