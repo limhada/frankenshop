3ee973310c7c5ceb7a1e417b0b7fa79186adf1b8
limhada <leem1315@gmail.com>
2023-08-22 13:05:45 +0900

    삭제기능 애니메이션 까지 완성

--- src/app/list/ListItem.tsx
+++ src/app/list/ListItem.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+// FIXME: 주석 다시 공부하고 정리 후 삭제하기
+
 import axios from 'axios';
 import { ObjectId } from 'mongodb';
 import Link from 'next/link';
+import { MouseEvent } from 'react';
 
 interface ListItemProps {
   result: {
@@ -18,60 +21,40 @@ export default function ListItem({ result }: ListItemProps) {
   return (
     <div>
       {result.map((el, i) => (
-        <div className='shadow-md bg-white rounded-md p-5 mb-3' key={i}>
+        <div
+          className='shadow-md bg-white rounded-md p-5 mb-3 opacity-100 transition-all duration-1000'
+          key={i}
+        >
           <Link href={'/detail/' + result[i]._id.toString()}>
             <h4 className='text-2xl font-bold m-0'>{el.title}</h4>
             <p className='text-gray-500 my-1 mx-0'>{el.content}</p>
           </Link>
           <Link href={'/edit/' + result[i]._id.toString()}>수정</Link>
           <span
-            onClick={() => {
-              // fetch('/api/post/delete', {
-              //   method: 'DELETE',
-              //   body: result[i]._id.toString(),
-              // })
-              //   .then((r) => {
-              //     if (r.status == 200) {
-              //       return r.json();
-              //     } else {
-              //       // 서버가 에러코드 전송시 실행할 코드
-              //       // ex)
-              //       // if(r.status == 500) {
-              //       //   서버통신 실패코드 500일때 실행할 코드 작성
-              //       // }
-              //     }
-              //   })
-              //   .then((r) => {
-              //     // ajax가 성공 시 실행할 코드
-              //     // console.log(r); // 서버가 보낸 json 확인
-              //   })
-              //   .catch((error) => {
-              //     console.log(error);
-              //   });
-
-
-              // 기존 fetch를 axios로 변경
+            onClick={(e: MouseEvent) => {
+              // const target = e.target as HTMLElement;
+              const target = (e.target as HTMLElement).parentElement;
               axios({
                 url: '/api/post/delete',
                 method: 'DELETE',
                 data: result[i]._id.toString(),
               })
                 .then((response) => {
-                  if (response.status === 200) {
-                    // ajax가 성공 시 실행할 코드
-                    console.log(response.data, "ListItem 서버 데이터 확인"); // 서버가 보낸 json 확인
-                  } else {
-                    // 서버가 에러코드 전송시 실행할 코드
-                    // ex) 서버통신 실패코드 500일때 실행할 코드 작성
-                    if (response.status === 500) {
-                      console.log("서버통신 실패코드 500에 대한 처리를 하세요.");
-                    }
+                  // console.log('확인~~~~~~~~~~1');
+                  if (response.status === 200 && target) {
+                    // if (target) {
+                      target.style.opacity = '0';
+                      // console.log('확인~~~~~~~~~~2');
+                      setTimeout(()=>{
+                        target.style.display = 'none';
+                      }, 1000)
+                    // }
+                    // console.log('확인~~~~~~~~~~3');
                   }
                 })
                 .catch((error) => {
                   console.log(error);
                 });
-
             }}
           >
             삭제
@@ -81,3 +64,5 @@ export default function ListItem({ result }: ListItemProps) {
     </div>
   );
 }
+
+// opacity가 0으로 바뀌지 않음 클릭시 삭제는 정상적으로 서버로 동작함
