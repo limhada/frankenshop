918c225dfbaeede1c755093897d52eee7069d51d
limhada <leem1315@gmail.com>
2023-09-04 16:35:44 +0900

    댓글 작성 버튼 클릭 시 댓글 입력 창 초기화

--- pages/api/comment/new.tsx
+++ pages/api/comment/new.tsx
@@ -20,16 +20,12 @@ export default async function handler(
         author_name: session.user.name,
       };
 
+      // 유저가 입력한 댓글과 유저의 정보, 해당 게시물 정보를 db에 저장
       const db = (await connectDB).db('frankenshop');
       await db.collection('comment').insertOne(saveData);
-
-      // let result = await db.collection('comment').find({ parent: new ObjectId(req.query._id as string) }).toArray()
-      let result = await db.collection('comment').find({ parent: new ObjectId(req.body._id) }).toArray()
-
       
-      // console.log(req.body._id,"~~~~~~~~~~~~~~~");
-      // console.log(result,"~~~~~~~~~~~~~~~~~~`");
-
+      // 부모 오브젝트id를 검색해서 게시물을 찾고 게시물의 최신 댓글 리스트를 다시 res에 넣어서 보내준다 -> 새로고침 없이 댓글 작성 시 화면 렌더링 하기 위함
+      let result = await db.collection('comment').find({ parent: new ObjectId(req.body._id) }).toArray()
       return res.status(200).json(result);
     } catch (error) {
       console.log(error, '서버에러');
--- src/app/detail/[id]/Comment.tsx
+++ src/app/detail/[id]/Comment.tsx
@@ -11,6 +11,9 @@ interface CommentData {
   content: string;
   author_name: string;
 }
+
+// TODO: 댓글 작성 버튼 클릭 시 댓글창에 기존에 입력 한 값 초기화 시키기
+
 export default function Comment({ _id }: CommentProps) {
   let [comment, setComment] = useState('');
 
@@ -24,8 +27,6 @@ export default function Comment({ _id }: CommentProps) {
     });
   }, []);
 
-  // console.log(data, "확인~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
   // TODO: 댓글 작성자 이름 보여주기
   // TODO: 로그인 상태 아니면 댓글작성 안보이게 그리고 댓글작성 만약 누르면 로그인 하라고 안내하기
 
@@ -33,39 +34,52 @@ export default function Comment({ _id }: CommentProps) {
     <div>
       <div>댓글목록</div>
 
-
-      { // 서버에서 응답받은 댓글 정보가 들어있는 data
-      data.length > 0 ? (
-        data.map((el, i) => {
-          return <p key={i}>작성자:{el.author_name} 내용:{el.content} </p>;
-        })
-      ) : (
-        <p>아직 댓글이 없습니다</p>
-      )}
+      {
+        // 서버에서 응답받은 댓글 정보가 들어있는 data
+        data.length > 0 ? (
+          data.map((el, i) => {
+            return (
+              <p key={i}>
+                작성자:{el.author_name} 내용:{el.content}{' '}
+              </p>
+            );
+          })
+        ) : (
+          <p>아직 댓글이 없습니다</p>
+        )
+      }
 
       <input
+        // value 속성을 사용하여 입력 필드의 값을 comment 상태와 동기화
+        value={comment}
         onChange={(e) => {
           setComment(e.target.value);
         }}
-        className='bg-gray-200'
+        className='commentInput bg-gray-200'
       />
       <button
         onClick={() => {
           // console.log(comment);
-          axios
-            .post('/api/comment/new', {
-              comment: comment,
-              _id: _id,
-            })
-            .then((r) => {
-              // console.log(r.data,"확인"); // 서버로부터 받은 데이터
-              setData(r.data) // 서버의 res에 들어있는 댓글 작성 클릭 시 전송된 댓글이 포함된 댓글 리스트를 업데이트 하기
+          if (comment === '') {
+            alert('내용을 입력하세요');
+          } else {
+            axios
+              .post('/api/comment/new', {
+                comment: comment,
+                _id: _id,
+              })
+              .then((r) => {
+                // console.log(r.data,"확인"); // 서버로부터 받은 데이터
+                setData(r.data); // 서버의 res에 들어있는 댓글 작성 클릭 시 전송된 댓글이 포함된 댓글 리스트를 업데이트 하기
 
-            })
-            .catch((error) => {
-              // 요청이 실패한 경우에 대한 처리
-              console.error(error);
-            });
+                // 댓글 작성 창 초기화
+                setComment('');
+              })
+              .catch((error) => {
+                // 요청이 실패한 경우에 대한 처리
+                console.error(error);
+              });
+          }
         }}
       >
         댓글 작성
