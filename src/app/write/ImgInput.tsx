d605dbad2fbbf6c85d39b099171815b79730eb43
limhada <leem1315@gmail.com>
2023-09-14 23:39:44 +0900

    S3 업로드 로직 fetch에서 axios로 변환완료

--- src/app/write/ImgInput.tsx
+++ src/app/write/ImgInput.tsx
@@ -30,50 +30,50 @@ export default function Write() {
             const blob = new Blob([file], { type: file.type });
             const url = URL.createObjectURL(blob);
             setCreateURL(url);
-
           }
         }}
       />
       <img src={createURL} />
-      <button 
-      type='submit'
-            className='px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600' onClick={ async() => {
-        
+      <button
+        type='submit'
+        className='px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600'
+        onClick={async () => {
+          // 한글로된 파일명이 깨지지 않게 하기 위해서
+          if (file) {
+            let filename = encodeURIComponent(file.name);
 
-
-            // 한글로된 파일명이 깨지지 않게 하기 위해서
-            if(file) {
-              let filename = encodeURIComponent(file.name);
+            try {
+              // 서버로 파일을 post 요청하기
+              const res = await axios.post(`/api/post/image?file=${filename}`);
+              console.log(res.data, "확인~~~~~~~~~~");
             
-
-            // 서버로 파일을 post 요청 하기
-            let res: Response = await fetch('/api/post/image?file=' + filename);
-            res = await res.json();
-
-            console.log(res, '확인~~~~~~~~~~');
-
-            // 서버에서 응답받은 데이터로 S3에 이미지 업로드 하기
-            // TODO: 블로그에 라이브러리 사용법 정리하기
-
-            //S3 업로드
-            const formData = new FormData();
-            Object.entries({ ...res.fields, file }).forEach(([key, value]) => {
-              formData.append(key, value as any); // 타입 에러로 인해 any 지정
-            });
-            let 업로드결과 = await fetch(res.url, {
-              method: 'POST',
-              body: formData,
-            });
-            console.log(업로드결과, '결과?????????????');
-
-            if (업로드결과.ok) {
-              setSrc(업로드결과.url + '/' + filename);
-            } else {
-              console.log('실패');
+              // 서버에서 응답받은 데이터로 S3에 이미지 업로드하기
+              // TODO: 블로그에 라이브러리 사용법 정리하기
+            
+              // S3 업로드
+              const formData = new FormData();
+              Object.entries({ ...res.data.fields, file }).forEach(([key, value]) => {
+                formData.append(key, value as any);
+              });
+            
+              const 업로드결과 = await axios.post(res.data.url, formData);
+              console.log(업로드결과, "결과?????????????");
+            
+              if (업로드결과.status === 200) {
+                setSrc(`${업로드결과.data.url}/${filename}`);
+              } else {
+                console.log("실패");
+              }
+            } catch (error) {
+              console.error("에러 발생:", error);
             }
+
           }
 
-      }}>글작성버튼</button>
+        }}
+      >
+        글작성버튼
+      </button>
     </div>
   );
 }
