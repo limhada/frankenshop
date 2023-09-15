a2aac8f95d1fba3198e36dcc3388c4819eeaaab5
limhada <leem1315@gmail.com>
2023-09-15 23:54:17 +0900

    imgInput 컴포넌트 부모 컴포넌트로 합침

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,39 +1,94 @@
-import { getServerSession } from 'next-auth';
-import { authOptions } from '../../../pages/api/auth/[...nextauth]';
-import LoginBtn from '../LoginBtn';
-import ImgInput from './ImgInput';
+'use client';
 
-export default async function Write() {
+import axios from 'axios';
+import { useState } from 'react';
+
+export default function Write() {
   // FIXME: 현재 로그인이 되어있지 않다면 로그인 페이지로 이동시키기
-  let session = await getServerSession(authOptions);
 
-  if (session === null) {
-    return (
-      <div>
-        로그인이 안되어있습니다
-        <br />
-        <LoginBtn></LoginBtn>
-      </div>
-    );
-  } else {
-    return (
-      <div className='p-20'>
-        <h4>글작성</h4>
-        <form action='/api/post/new' method='POST'>
+  const [createURL, setCreateURL] = useState('');
+  const [file, setFile] = useState<File>(); // 이미지 파일 상태 추가
+  // let [src, setSrc] = useState('');
+
+  return (
+    <div className='p-20'>
+      <h4>글작성</h4>
+      <form action='/api/post/new' method='POST'>
+        <input
+          name='title'
+          placeholder='글 제목'
+          className='className="block w-full p-2 mb-2 border rounded-lg"'
+        />
+        <input
+          name='content'
+          placeholder='글 내용'
+          className='className="block w-full p-2 mb-2 border rounded-lg"'
+        />
+
+
+        <div className='p-20'>
+          <h4>글작성</h4>
           <input
-            name='title'
-            placeholder='글 제목'
-            className='className="block w-full p-2 mb-2 border rounded-lg"'
+            type='file'
+            accept='image/*'
+            onChange={async (e) => {
+              let file = e.target.files?.[0];
+              if (file) {
+                // 파일을 화면에 렌더링해주는 기능
+                setFile(file);
+                const blob = new Blob([file], { type: file.type });
+                const url = URL.createObjectURL(blob);
+                setCreateURL(url);
+              }
+            }}
           />
-          <input
-            name='content'
-            placeholder='글 내용'
-            className='className="block w-full p-2 mb-2 border rounded-lg"'
-          />
-          <ImgInput/>
-          
-        </form>
-      </div>
-    );
-  }
+          <img src={createURL} />
+          <button
+            type='submit'
+            className='px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600'
+            onClick={async () => {
+              // 한글로된 파일명이 깨지지 않게 하기 위해서
+              if (file) {
+                let filename = encodeURIComponent(file.name);
+
+                try {
+                  // 서버로 파일을 post 요청하기
+                  const res = await axios.post(
+                    `/api/post/image?file=${filename}`
+                  );
+                  console.log(res.data, '확인~~~~~~~~~~');
+
+                  // 서버에서 응답받은 데이터로 S3에 이미지 업로드하기
+                  // TODO: 블로그에 라이브러리 사용법 정리하기
+
+                  // S3 업로드
+                  const formData = new FormData();
+                  Object.entries({ ...res.data.fields, file }).forEach(
+                    ([key, value]) => {
+                      formData.append(key, value as any);
+                    }
+                  );
+
+                  const 업로드결과 = await axios.post(res.data.url, formData);
+                  console.log(업로드결과, '결과?????????????');
+
+                  if (업로드결과.status === 200) {
+                    // setSrc(`${업로드결과.data.url}/${filename}`);
+                    // TODO: s3에 업로드한 이미지 몽고db에 저장하기 그리고 게시물 리스트에서 게시물 클릭시 해당 게시물의 내용과 함께 이미지도 보여주기
+                    
+                  } else {
+                    console.log('실패');
+                  }
+                } catch (error) {
+                  console.error('에러 발생:', error);
+                }
+              }
+            }}
+          >
+            글작성버튼
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
