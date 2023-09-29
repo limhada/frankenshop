bc8d3635bfc05a7264a6399ed3117fe90bf0f97a
limhada <leem1315@gmail.com>
2023-09-29 23:39:38 +0900

    메인페이지 내용추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -2,7 +2,13 @@ export default function Mypage() {
   return (
     <div>
       <h1>마이 페이지</h1>
-      <div>내 정보 수정</div>
+      <div>내 정보 수정
+        <div>아이디(이메일)</div>
+        <div>이름</div>
+        <div>휴대폰 번호</div>
+        <div>비밀번호 변경</div>
+        <div>배송지</div>
+      </div>
       <div>주문/배송</div>
       <div>장바구니</div>
       <div>리뷰작성</div>
