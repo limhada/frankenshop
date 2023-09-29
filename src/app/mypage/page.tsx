a670f6e0792cda4098232acf5498e0f8c6cbf454
limhada <leem1315@gmail.com>
2023-09-29 23:37:09 +0900

    my페이지 항목추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -2,7 +2,13 @@ export default function Mypage() {
   return (
     <div>
       <h1>마이 페이지</h1>
-      
+      <div>내 정보 수정</div>
+      <div>주문/배송</div>
+      <div>장바구니</div>
+      <div>리뷰작성</div>
+      <div>보유쿠폰</div>
+      <div>포인트</div>
+      <div>Q&A</div>
     </div>
   );
 }
