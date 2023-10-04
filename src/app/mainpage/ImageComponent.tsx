61698e107d03e6e26531c6af6e6e49dc4e171182
limhada <leem1315@gmail.com>
2023-10-04 21:02:12 +0900

    유즈이펙트로 무한슬라이드 구현

--- src/app/mainpage/ImageComponent.tsx
+++ src/app/mainpage/ImageComponent.tsx
@@ -29,12 +29,17 @@ export default function ImageComponent(): React.ReactElement {
       '/imgtest/5.jpeg',
     ],
   };
+  const imageList = [
+    imgArr.imgtest[imgArr.imgtest.length - 1],
+    ...imgArr.imgtest,
+    imgArr.imgtest[0],
+  ];
 
-  const [num, setNum] = useState<number>(0);
+  const [currentImgIndex, setCurrentImgIndex] = useState<number>(1); // useEffect로 첫번째 사진일 경우 마지막으로 이동하기 때문에 1로 해야 원하는 첫번째 이미지가 나타남
   const [dragging, setDragging] = useState<boolean>(false);
   const [startX, setStartX] = useState<number>(0);
   const [offsetX, setOffsetX] = useState<number>(0);
-
+  const [num, setNum] = useState(700);
   // TODO: 자동 슬라이드 정리하기
   /*
 
@@ -60,13 +65,13 @@ export default function ImageComponent(): React.ReactElement {
       // 컴포넌트 언마운트 시 타이머 클리어
       clearInterval(timer);
     };
-  }, [num, dragging, autoSlide]);
+  }, [currentImgIndex, dragging, autoSlide]);
 
   const handleNextSlide = (): void => {
-    if (num < imgArr['imgtest'].length - 1) {
-      setNum(num + 1);
+    if (currentImgIndex < imageList.length - 1) {
+      setCurrentImgIndex(currentImgIndex + 1);
     } else {
-      setNum(0);
+      setCurrentImgIndex(0);
     }
   };
 
@@ -75,6 +80,30 @@ export default function ImageComponent(): React.ReactElement {
   };
 
   */
+  const [style, setStyle] = useState({
+    transform: `translateX(-${currentImgIndex}00%)`,
+    transition: `all 0.4s ease-in-out`,
+  });
+  useEffect(() => {
+    const handleSlide = () => {
+      if (currentImgIndex === 0) {
+        setCurrentImgIndex(imageList.length - 2);
+        // setStyle({
+        //   transform: `translateX(-${(imageList.length - 2) * 100}%)`,
+        //   transition: '0ms',
+        // });
+      } else if (currentImgIndex >= imageList.length - 1) {
+        setCurrentImgIndex(1);
+        // setStyle({
+        //   transform: `translateX(-${1 * 100}%)`,
+        //   transition: '0ms',
+        // });
+      }
+    };
+
+    const timer = setTimeout(handleSlide, 50);
+    return () => clearTimeout(timer);
+  }, [currentImgIndex, imageList]);
 
   const handleMouseDown = (e: React.MouseEvent): void => {
     setDragging(true);
@@ -90,18 +119,18 @@ export default function ImageComponent(): React.ReactElement {
   const handleMouseUp = (): void => {
     setDragging(false);
     if (offsetX > 50) {
-      if (num > 0) {
-        setNum(num - 1);
+      if (currentImgIndex > 0) {
+        setCurrentImgIndex(currentImgIndex - 1);
       } else {
         // 이미지가 처음일 때 왼쪽으로 이동하면 맨 마지막 이미지로 이동
-        setNum(imgArr['imgtest'].length - 1);
+        setCurrentImgIndex(imageList.length - 1);
       }
     } else if (offsetX < -50) {
-      if (num < imgArr['imgtest'].length - 1) {
-        setNum(num + 1);
+      if (currentImgIndex < imageList.length - 1) {
+        setCurrentImgIndex(currentImgIndex + 1);
       } else {
         // 이미지가 마지막일 때 오른쪽으로 이동하면 첫 번째 이미지로 이동
-        setNum(0);
+        setCurrentImgIndex(0);
       }
     }
     setOffsetX(0);
@@ -128,9 +157,9 @@ export default function ImageComponent(): React.ReactElement {
       <div
         className='relative w-full md:w-[700px] h-0 pb-[120%] md:pb-[110%] overflow-hidden shadow-md rounded-md'
         onMouseMove={handleMouseMove}
-        onTouchMove={handleTouchMove}
+        // onTouchMove={handleTouchMove}
       >
-        {imgArr['imgtest'].map((src: string, index: number) => (
+        {imageList.map((src: string, index: number) => (
           <Image
             key={index}
             src={src}
@@ -139,19 +168,19 @@ export default function ImageComponent(): React.ReactElement {
             height={770}
             quality={100}
             className={`absolute top-0 left-0 w-full h-full ${
-              dragging ? '' : 'transition-transform duration-700'
+              dragging ? '' : `transition-transform duration-${num}`
             }`}
             style={{
               transform: ((): string => {
-                if (num === index) {
+                if (currentImgIndex === index) {
                   return `translateX(${offsetX}px)`;
-                } else if (index === num + 1) {
+                } else if (index === currentImgIndex + 1) {
                   return `translateX(calc(100% + ${offsetX}px))`;
-                } else if (index < num) {
+                } else if (index < currentImgIndex) {
                   return `translateX(calc(-100% + ${offsetX}px))`;
                 } else {
                   return 'translateX(100%)';
-                  // num < index인 나머지 이미지를 오른쪽으로 이동
+                  // currentImgIndex < index인 나머지 이미지를 오른쪽으로 이동
                 }
               })(),
               // objectFit: "contain", // 이미지를 원본사이즈로
@@ -159,7 +188,7 @@ export default function ImageComponent(): React.ReactElement {
             }}
             onMouseDown={handleMouseDown}
             onMouseUp={handleMouseUp}
-            onMouseLeave={handleMouseUp}
+            // onMouseLeave={handleMouseUp}
             onTouchStart={handleTouchStart}
             onTouchMove={handleTouchMove}
             onTouchEnd={handleTouchEnd}
@@ -174,24 +203,24 @@ export default function ImageComponent(): React.ReactElement {
           
           `}
           onClick={() => {
-            if (num > 0) {
-              setNum(num - 1);
+            if (currentImgIndex > 0) {
+              setCurrentImgIndex(currentImgIndex - 1);
             } else {
-              setNum(imgArr['imgtest'].length - 1);
+              setCurrentImgIndex(imageList.length - 1);
             }
           }}
         >
           &lt;
         </button>
         {/* 이미지 번호 출력 */}
-        {num + 1} / {imgArr['imgtest'].length}
+        {currentImgIndex + 1} / {imageList.length}
         <button
           className={`w-10 h-10 `}
           onClick={() => {
-            if (num < imgArr['imgtest'].length - 1) {
-              setNum(num + 1);
+            if (currentImgIndex < imageList.length - 1) {
+              setCurrentImgIndex(currentImgIndex + 1);
             } else {
-              setNum(0);
+              setCurrentImgIndex(0);
             }
           }}
         >
