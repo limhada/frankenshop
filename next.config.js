421ef100a24535c9cda1946da47ff9d2b2c91b01
limhada <leem1315@gmail.com>
2023-10-11 18:02:24 +0900

    이미지 도메인 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['github.com'], // 이미지를 불러올 호스트(도메인)를 배열로 설정
+  },
+}
 
 module.exports = nextConfig
