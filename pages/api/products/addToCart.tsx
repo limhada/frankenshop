8383bda98a6d901bc42c136f4239fb7eefbd032d
limhada <leem1315@gmail.com>
2023-10-14 22:46:53 +0900

    몽고db cart 컬렉션 에 해당 아이템 추가 및 추가되어 있을 때 클릭 시 1개 수량 증가

--- pages/api/products/addToCart.tsx
+++ pages/api/products/addToCart.tsx
@@ -1,28 +1,39 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { connectDB } from '../../../util/database';
 import { ObjectId } from 'mongodb';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'POST') {
-    console.log(req.body._id); // req.body -> 현재 제품의 _id
+    // console.log(req.body._id); // req.body -> 현재 제품의 _id
+
+    let session = await getServerSession(req, res, authOptions);
+    console.log('회원정보 확인~~~~~~~~~~~~', session.user.email);
 
     const insertData = {
-      parent: new ObjectId(req.body._id),
+      contents: new ObjectId(req.body._id),
+      email: session.user.email,
+      quantity: 1,
     };
 
     try {
       const db = (await connectDB).db('frankenshop');
 
       let result = await db
-        .collection('contents')
-        .find({ _id: new ObjectId(req.body._id) })
+        .collection('cart')
+        .find({ contents: new ObjectId(req.body._id) })
         .toArray();
       console.log('ㅎㅇ~~~~~~~~~~', result);
       if (result.length === 0) {
-        console.log('없음');   
+        let result2 = await db.collection('cart').insertOne(insertData);
+        console.log('처음 데이터 추가 완료');
+      } else if (result.length > 0) {
+        const updateResult = await db.collection('cart').updateOne({ contents: new ObjectId(req.body._id) }, { $inc: { quantity: 1 } });
+        console.log("카트값 1증가 성공");
       }
 
       return res.status(200).json('성공');
