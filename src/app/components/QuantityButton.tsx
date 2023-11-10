81264274e3846997744151e2882104ce779a98ab
limhada <leem1315@gmail.com>
2023-11-10 23:54:01 +0900

    할일추가

--- src/app/components/QuantityButton.tsx
+++ src/app/components/QuantityButton.tsx
@@ -1,23 +1,22 @@
-'use client'
+'use client';
 
-import { useState } from "react"
+import { useState } from 'react';
 
 export default function QuantityButton() {
-
-  const [quantity, setQuantity] = useState(1)
+  const [quantity, setQuantity] = useState(1);
 
   const handleQuantityChange = (num: number) => {
     // 수량 1미만으로 감소하지 않게
     if (num === 1 || (num === -1 && quantity > 1)) {
       setQuantity(quantity + num);
     }
-  }
+  };
 
   return (
     <div>
       <button onClick={() => handleQuantityChange(1)}>+</button>
-              <div>수량: {quantity}</div>
-              <button onClick={() => handleQuantityChange(-1)}>-</button>
+      <div>수량: {quantity}</div>
+      <button onClick={() => handleQuantityChange(-1)}>-</button>
     </div>
-  )
+  );
 }
