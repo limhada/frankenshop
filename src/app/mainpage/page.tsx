736620983602b899b81348786bd25e20312555e2
limhada <leem1315@gmail.com>
2024-01-07 22:57:54 +0900

    배포 에러 수정 -7

--- src/app/mainpage/page.tsx
+++ src/app/mainpage/page.tsx
@@ -1,8 +1,8 @@
 // FIXME: 배포에러 수정2
 // import Contents from './contents';
 import Contents from '../../../src/app/mainpage/contents';
-// import ImageComponent from './imageComponent';
-import ImageComponent from '../../../src/app/mainpage/imageComponent';
+import ImageComponent from './imageComponent';
+// import ImageComponent from '../../../src/app/mainpage/imageComponent';
 
 // export interface ListProps {
 //   _id: ObjectId;
