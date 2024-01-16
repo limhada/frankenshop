d9de27851aa7790ce863d991b53a82b6e2e38599
limhada <leem1315@gmail.com>
2024-01-17 01:56:38 +0900

    정규표현식에서 특수 문자를 이스케이핑하여 문자열이 정확하게 검색되도록 함 . 입력시 모든 데이터가 검색되는 이슈 해결

--- src/app/test3/page.tsx
+++ src/app/test3/page.tsx
@@ -38,6 +38,9 @@ const list = [
   // '샤인머스켓',
   // '초코송이',
   '고구마',
+  '1.2',
+  '1..2',
+  '1.2.3'
   
 ];
 
@@ -51,14 +54,23 @@ const combine = (cho: any, jung: any, jong: any) => {
   );
 };
 
+// 이스케이핑 함수
+const escapeRegExp = (text: string) => {
+  // 정규표현식에서 특수 문자를 이스케이핑하여 문자열이 정확하게 검색되도록 함
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+};
+
 const makeRegexByCho = (search = '') => {
+// 입력값을 이스케이핑하여 특수문자가 정확히 검색되도록 함
+  const escapedSearch = escapeRegExp(search);
   const regex = CHO_HANGUL.reduce(
     (acc, cho, index) =>
+      // 정규표현식 내에서 특정 자음을 범위로 나타내도록 함
       acc.replace(
         new RegExp(cho, 'g'),
         `[${combine(index, 0, 0)}-${combine(index + 1, 0, -1)}]`
       ),
-    search
+    escapedSearch
   );
 
   return new RegExp(`(${regex})`);
@@ -73,8 +85,24 @@ const ChoSearch = () => {
 
     const trimmedInputValue = inputValue.trim();  // 필요한 경우에만 trim() 호출
 
+    // TODO: 예외처리 블로그에 정리하기
+
     // 처음 입력값이 공백인 경우 입력 안되도록 처리
     if (search === '' && trimmedInputValue === '') {
+      return;
+    }
+
+    // 입력값이 '.' 하나거나 '.'로만 이루어진 경우 동작하지 않음
+    // if (/^\.+$/.test(trimmedInputValue)) {
+    //   setSearch(inputValue);
+    //   setResult([]);
+    //   return;
+    // }
+
+    // 입력값이 모두 삭제된 경우 동작하지 않음
+    if (search !== '' && trimmedInputValue === '') {
+      setSearch(inputValue);
+      setResult([]);
       return;
     }
 
